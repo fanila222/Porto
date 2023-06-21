2e64896b43df846d0ec33349cfefca6ab92e8963
fanila222 <mirfanfadhila3.com>
2023-06-22 00:14:09 +0700

    icon, button grad, skill section

---
+++ js/style.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    $(".skill-button").click(function(){
+        let skillAttr = $(this).attr("skill-attribute");
+    
+        $(".skill-img-wrapper img").each(function(){
+            if ($(this).attr("skill-attribute") == skillAttr) {
+                $(this).fadeIn();
+            }else{
+                $(this).fadeOut();
+            }
+        })
+    
+        if ($(this).hasClass("active")) {
+            $(this).removeClass("active");
+            $(".skill-img-wrapper img").fadeIn();
+        }else{
+            $(".skill-button").removeClass("active")
+            $(this).addClass("active")
+        }
+    })
+});
